1fe389793f35804af2e39059167a1b50ddedd0a6
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-07-08 23:42:47 +0200

    Add files via upload

--- dotfiles/.config/i3/scripts/i3blocks_net_wlan.sh
+++ dotfiles/.config/i3/scripts/i3blocks_net_wlan.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # Author: AISK11
-# Description: This script calculates network throughput for interface "wlan0" in b/s every second.
+# Description: This script calculates network throughput for interface "eth0" in b/s every second.
 # 1 MB/s = 8 Mb/s
 # 1 Mb/s = 1,000 kb/s = 1,000,000 b/s
 # Created for: i3blocks
 # Dependencies: iproute2 wireless-tools
 
-# interfaces=<wlan0|wlp0s20f3>
+# interfaces=<eth0|wlan0|lo>
 INTERFACE="wlan0"
 INTERFACE_STATE=$(ip a | grep ${INTERFACE} | grep -v "inet" | cut -d' ' -f9)
 
-CHANNEL=$(iwlist ${INTERFACE} channel | grep "Current" | tr -s " " | cut -d " " -f 3,4 | tr "=" ":" | cut -d ":" -f2)
+# Channel is not workign with i3blocks :(
+#CHANNEL=$(iwconfig ${INTERFACE} | egrep -o "[0-9].*GHz")
 
 if [ "${INTERFACE_STATE}" = "DOWN" ]
 then
@@ -45,7 +46,7 @@ then
         elif [ ${#TX_BITS} -eq 2 ]; then
             TX_BITS=" ${TX_BITS}"
         fi 
-        echo "RX ${RX_BITS} Mb/s; TX ${TX_BITS} Mb/s (${CHANNEL})"
+        echo "RX ${RX_BITS} Mb/s; TX ${TX_BITS} Mb/s"
     elif [ "${TX_BITS}" -ge "1000" ] || [ "${RX_BITS}" -ge "1000" ]
     then
         RX_BITS=$(( ${RX_BITS} / 1000 )) 
@@ -61,7 +62,7 @@ then
         elif [ ${#TX_BITS} -eq 2 ]; then
             TX_BITS=" ${TX_BITS}"
         fi 
-        echo "RX ${RX_BITS} Mb/s; TX ${TX_BITS} Mb/s (${CHANNEL})"
+        echo "RX ${RX_BITS} Mb/s; TX ${TX_BITS} Mb/s"
     else
         # Add leading space in front of variable:
         if [ ${#RX_BITS} -eq 1 ]; then
@@ -74,7 +75,7 @@ then
         elif [ ${#TX_BITS} -eq 2 ]; then
             TX_BITS=" ${TX_BITS}"
         fi 
-        echo "RX ${RX_BITS} Mb/s; TX ${TX_BITS} Mb/s (${CHANNEL})"
+        echo "RX ${RX_BITS} Mb/s; TX ${TX_BITS} Mb/s"
     fi
 else
     printf "UNKNOWN\n"
