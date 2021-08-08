ec2b0f8bac3cc3e9e1e0ae9bcd8087bbcbebf73d
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-08-08 21:09:12 +0200

    Update run_wlan0.sh

--- scripts/run_wlan0.sh
+++ scripts/run_wlan0.sh
@@ -8,11 +8,14 @@
 # Usage: this is personal script that run wlan0 interface and require additional config to work
 ##################################################################
 
+INTERFACE="wlan0"
+
 doas rfkill unblock wlan
-doas macchanger -Ab wlan0
+doas macchanger -Ab ${INTERFACE}
 doas rm -rf /var/lib/dhcpcd/*
-doas ip l set wlan0 up
+doas ip l set ${INTERFACE} up
+doas rm -f /run/wpa_supplicant/${INTERFACE}
 doas systemctl start wpa_supplicant.service
 doas systemctl start dhcpcd.service
 doas wpa_supplicant -B -D wext -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
-doas dhcpcd wlan0
+doas dhcpcd ${INTERFACE}
