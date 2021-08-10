081369b905b005f9a26379e8cd298fa5ba960430
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-08-10 13:28:05 +0000

    Add files via upload

--- scripts/run_wlan0.sh
+++ scripts/run_wlan0.sh
@@ -10,12 +10,20 @@
 
 INTERFACE="wlan0"
 
-doas rfkill unblock wlan
-doas macchanger -Ab ${INTERFACE}
-doas rm -rf /var/lib/dhcpcd/*
-doas ip l set ${INTERFACE} up
-doas rm -f /run/wpa_supplicant/${INTERFACE}
-doas systemctl start wpa_supplicant.service
-doas systemctl start dhcpcd.service
-doas wpa_supplicant -B -D wext -i ${INTERFACE} -c /etc/wpa_supplicant/wpa_supplicant.conf
-doas dhcpcd ${INTERFACE}
+# bring everything down in case of a restart
+doas ip l set ${INTERFACE} down && echo "[+] ${INTERFACE} is DOWN." || echo "[-] ERROR while putting ${INTERFACE} DOWN!"
+doas dhcpcd --release ${INTERFACE} && echo "[+] DHCP released for ${INTERFACE}." || echo "[-] ERROR while releasing DHCP!"
+doas ip a flush ${INTERFACE} && echo "[+] IP flushed for ${INTERFACE}." || echo "[-] ERROR while flushing IP for ${INTERFACE}!"
+doas systemctl stop dhcpcd.service && echo "[+] dhcpcd.service stopped." || echo "[-] ERROR while stopping dhcpcd.service!"
+doas systemctl stop wpa_supplicant.service && echo "[+] wpa_supplicant.service stopped." || echo "[-] ERROR while stopping wpa_supplicant.service!"
+
+# start everything
+doas rfkill unblock wlan && echo "[+] wlan unblocked in rfkill." || echo "[-] ERROR while unblocking wlan in rfkill!"
+doas macchanger -A ${INTERFACE} && echo "[+] MAC changed for ${INTERFACE}." || echo "[-] ERROR while changing MAC address for ${INTERFACE}!" 
+doas rm -rf /var/lib/dhcpcd/* && echo "[+] All leased addresses were removed in '/var/lib/dhcpcd/'." || echo "[-] ERROR while removing leased addresses in '/var/lib/dhcpcd/'!"
+doas ip l set ${INTERFACE} up && echo "[+] ${INTERFACE} is UP." || echo "[-] ERROR while putting ${INTERFACE} UP!"
+doas rm -f /run/wpa_supplicant/${INTERFACE} && echo "[+] removed '/run/wpa_supplicant/${INTERFACE}." || echo "[-] ERROR while removing '/run/wpa_supplicant/${INTERFACE}!"
+doas systemctl start wpa_supplicant.service && echo "[+] wpa_supplicant.service started." || echo "[-] ERROR while starting wpa_supplicant.service!"
+doas systemctl start dhcpcd.service && echo "[+] dhcpcd.service started." || echo "[-] ERROR while starting dhcpcd.service!"
+doas wpa_supplicant -B -D wext -i ${INTERFACE} -c /etc/wpa_supplicant/wpa_supplicant.conf && echo "[+] Connected to AP according to '/etc/wpa_supplicant/wpa_supplicant.conf'." || echo "[-] ERROR could not connect to AP according to '/etc/wpa_supplicant/wpa_supplicant.conf'!"
+doas dhcpcd ${INTERFACE} && echo "[+] DHCP discovery for ${INTERFACE}." || echo "[-] ERROR while DHCP discovery for ${INTERFACE}!"
