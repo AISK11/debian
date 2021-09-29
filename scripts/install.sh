57bd153b86d6c25aa330b4c4f7382d1525334dab
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-09-29 04:52:02 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -51,6 +51,7 @@ sed -i 's/GRUB_DEFAULT=.*/GRUB_DEFAULT=0/g' /etc/default/grub &&
 sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=1/g' /etc/default/grub &&
 sed -i 's/GRUB_TIMEOUT_STYLE=.*/GRUB_TIMEOUT_STYLE=menu/g' /etc/default/grub &&
 sed -i 's/GRUB_DISABLE_RECOVERY=.*/GRUB_DISABLE_RECOVERY=true/g' /etc/default/grub &&
+sed -i 's/#GRUB_DISABLE_RECOVERY/GRUB_DISABLE_RECOVERY/g' /etc/default/grub &&
 ## GRUB colors:
 echo -e "set color_normal=white/black" > /boot/grub/custom.cfg &&
 echo -e "set color_highlight=black/white" >> /boot/grub/custom.cfg &&
