feeacd9e4730891228f3103d50566e627364be0d
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-07 22:36:36 +0100

    Add files via upload

--- install/subscripts/system-disable-pcspkr.sh
+++ install/subscripts/system-disable-pcspkr.sh
@@ -2,9 +2,23 @@
 
 ## This script disables pcspkr (that annoying debian beeping).
 
-## Disable speaker bell:
-echo -e "set bell-style none" >> /etc/inputrc &&
-echo -e "blacklist pcspkr" > /etc/modprobe.d/blacklist.conf &&
+### Disable speaker bell:
+sed -i "s/^# set bell-style none/set bell-style none/" /etc/inputrc &&
+
+### blacklist pcspkr module:
+## If file does not exists:
+if [[ ! -e /etc/modprobe.d/blacklist.conf ]]; then
+    echo -e "blacklist pcspkr" > /etc/modprobe.d/blacklist.conf
+## This file should not be directory:
+elif [[ -d /etc/modprobe.d/blacklist.conf ]]; then
+    rm -rf /etc/modprobe.d/blacklist.conf
+    echo -e "blacklist pcspkr" > /etc/modprobe.d/blacklist.conf
+## If file exists, but does not contain "blacklist pcspkr":
+## grep returns 1 if does not find match:
+elif grep "^blacklist pcspkr$"; then
+    echo -e "blacklist pcspkr" >> /etc/modprobe.d/blacklist.conf
+
+### Generate modules.dep and map files:
 depmod -a &> /dev/null &&
 update-initramfs -u &> /dev/null &&
 echo -e "\n[+] pcspkr disabled." || echo -e "\n[-] ERROR! pcspkr could not be disabled!"
