0c228990bc76894cb2df9af1c378d1b9c71b4023
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-08 03:45:31 +0100

    Add files via upload

--- install/subscripts/app-zsh.sh
+++ install/subscripts/app-zsh.sh
@@ -8,4 +8,4 @@ USER="${1}"
 ## Install ZSH and ZSH related pkgs:
 apt install zsh zsh-autosuggestions zsh-syntax-highlighting -y &> /dev/null &&
 usermod -s /bin/zsh ${USER} &> /dev/null &&
-echo -e "\n[+] ZSH related packages were installed and ZSH was set as default shell for user '${USER}'" || echo -e "\n[-] ERROR! Either ZSH related packages were not installed or ZSH was not set as default shell for user '${USER}'!"
+echo -e "\n[+] ZSH related packages were installed and ZSH was set as default shell for user '${USER}.'" || echo -e "\n[-] ERROR! Either ZSH related packages were not installed or ZSH was not set as default shell for user '${USER}'!"
