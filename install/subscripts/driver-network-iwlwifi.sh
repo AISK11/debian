446cb4998ea26cdbd41a1fe485b2291a2fa175c5
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-08 01:39:01 +0100

    Add files via upload

--- install/subscripts/driver-network-iwlwifi.sh
+++ install/subscripts/driver-network-iwlwifi.sh
@@ -4,5 +4,5 @@
 ## Requires: non-free and contrib packages.
 
 ## Driver for iwlwifi:
-apt install firmware-iwlwifi -y 2> /dev/null &&
+apt install firmware-iwlwifi -y &> /dev/null &&
 echo -e "\n[+] Installed firmware for iwlwifi." || echo -e "\n[-] ERROR! Could not  install iwlwifi driver!"
