333006af6acb493e55d6f34b8cc3356a7291287d
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-09 10:41:13 +0100

    Add files via upload

--- install/subscripts/net-wpasupplicant.sh
+++ install/subscripts/net-wpasupplicant.sh
@@ -5,7 +5,7 @@
 ## Install wpasupplicant package with additional wireless tools:
 apt install wpasupplicant wireless-tools -y &> /dev/null &&
 ## Copy wpa_supplicant template to right location:
-cp ./files/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf &&
+cp ../config_files/WI-FI/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf &&
 ## Change rights for file to be unreadable by normal users:
 chmod 0600 /etc/wpa_supplicant/wpa_supplicant.conf &&
 echo -e "\n[+] Installed and set up wpasupplicant template in '/etc/wpa_supplicant/wpa_supplicant.conf'." || echo -e "\n[-] ERROR! Could not install 'wpasupplicant' or set up template file '/etc/wpa_supplicant/wpa_supplicant.conf'!"
