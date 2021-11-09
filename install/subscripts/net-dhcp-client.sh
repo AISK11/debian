9db1400fa3b8a7c96372b95422c5b5479d27a124
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-09 10:37:30 +0100

    Add files via upload

--- install/subscripts/net-dhcp-client.sh
+++ install/subscripts/net-dhcp-client.sh
@@ -7,7 +7,7 @@ apt install dhcpcd5 -y &> /dev/null &&
 ## Do not start DHCPCD on startup:
 systemctl disable dhcpcd.service &> /dev/null &&
 ## Copy DHCPCD config:
-cp ./files/dhcpcd.conf /etc/dhcpcd.conf &&
+cp ../config_files/DHCP/dhcpcd.conf /etc/dhcpcd.conf &&
 ## Uninstall previous DHCP Client:
 dpkg --purge isc-dhcp-client isc-dhcp-common &> /dev/null &&
 echo -e "\n[+] DHCPCD5 installed and set up in '/etc/dhpcd.conf'." || echo -e "\n[-] ERROR! DHCPCD5 could not be installed and set up in '/etc/dhcpcd.conf'!"
