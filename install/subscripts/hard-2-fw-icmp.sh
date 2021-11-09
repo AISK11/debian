cf4ad4117188a8c66a7677214510e94aef7996fd
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-09 22:59:00 +0100

    Add files via upload

--- install/subscripts/hard-2-fw-icmp.sh
+++ install/subscripts/hard-2-fw-icmp.sh
@@ -10,11 +10,26 @@ apt install iptables iptables-persistent -y 2> /dev/null &&
 iptables -F &&
 ## Delete all non-default chains:
 iptables -X &&
+## Zero all counters:
+iptables -Z &&
 ## Copy persistent FW rules:
 cp ../config_files/FW/iptables/rules.v4 /etc/iptables/rules.v4 &&
 ## Copy special logging:
 cp ../config_files/Logging/Rsyslog/iptables.conf /etc/rsyslog.d/iptables.conf &&
 cp ../config_files/Logging/Logrotate/iptables /etc/logrotate.d/iptables &&
+
+## Create IPv6 FW rules:
+## Delete all rules:
+ip6tables -F &&
+## Delete all non-default chains:
+ip6tables -X &&
+## Zero all counters:
+ip6tables -Z &&
+## Copy persistent FW rules:
+cp ../config_files/FW/iptables/rules.v6 /etc/iptables/rules.v6 &&
+## Copy special logging:
+cp ../config_files/Logging/Rsyslog/ip6tables.conf /etc/rsyslog.d/ip6tables.conf &&
+cp ../config_files/Logging/Logrotate/ip6tables /etc/logrotate.d/ip6tables &&
 
 ## Add crontab rules to root's cron:
 (crontab -l &> /dev/null; echo "@reboot systemctl restart logrotate.service && systemctl restart rsyslog.service") | crontab - &&
