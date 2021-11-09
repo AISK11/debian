3462076226eafcfefa37876323da7f45dd914d48
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-09 10:40:16 +0100

    Add files via upload

--- install/subscripts/net-dns-servers.sh
+++ install/subscripts/net-dns-servers.sh
@@ -2,5 +2,5 @@
 
 ## This script sets DNS servers:
 
-cp ./files/resolv.conf /etc/resolv.conf &&
+cp ../config_files/DNS/resolv.conf /etc/resolv.conf &&
 echo -e "\n[+] DNS servers set in '/etc/resolv.conf'." || echo -e "ERROR! DNS servers could not be set in '/etc/resolv.conf'!"
