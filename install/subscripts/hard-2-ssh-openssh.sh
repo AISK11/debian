17064363adab172eb1c02f0f26c48ad14b086b57
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-10 16:24:23 +0100

    Add files via upload

--- install/subscripts/hard-2-ssh-openssh.sh
+++ install/subscripts/hard-2-ssh-openssh.sh
@@ -9,4 +9,4 @@ systemctl disable ssh.service &> /dev/null &&
 systemctl stop ssh.service &> /dev/null &&
 ## Copy SSHD settings:
 cp ../config_files/SSH/sshd_config /etc/ssh/sshd_config &&
-echo -e "\n[+] Open-SSH server was successfully installed and configured." || echo -e "\n[-] ERROR! Open-SSH server could not be successfully installed!"
+echo -e "\n[+] Open-SSH server was successfully installed and configured in '/etc/ssh/sshd_config/'." || echo -e "\n[-] ERROR! Open-SSH server could not be successfully installed in '/etc/ssh/sshd_config'!"
