4b95c30aa72d65c476331aee366dc900ae47b527
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-10 16:33:17 +0100

    Update net-disable-hotplug.sh

--- install/subscripts/net-disable-hotplug.sh
+++ install/subscripts/net-disable-hotplug.sh
@@ -8,7 +8,7 @@ declare -a INTERFACE_LIST=("eth0" "wlan0")
 ## Loopback is specific interface and it should autostart:
 echo -e "auto lo" > /etc/network/interfaces &&
 echo -e "iface lo inet loopback" >> /etc/network/interfaces &&
-echo -e "\n[+] Loopback interface 'lo' was successfully set up in '/etc/network/interfaces'" || echo -e "\n[-] ERROR! Loopback interface 'lo0' could not be set up!"
+echo -e "\n[+] Loopback interface 'lo' was successfully set up in '/etc/network/interfaces'." || echo -e "\n[-] ERROR! Loopback interface 'lo0' could not be set up!"
 
 ## Disable interfaces for auto connectivity:
 for INTERFACE in "${INTERFACE_LIST[@]}"; do
