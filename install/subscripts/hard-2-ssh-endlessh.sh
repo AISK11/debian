9d957117fca910a674abcbef132ea9f98d4f318b
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-10 15:57:46 +0100

    Add files via upload

--- install/subscripts/hard-2-ssh-endlessh.sh
+++ install/subscripts/hard-2-ssh-endlessh.sh
@@ -9,4 +9,4 @@ systemctl disable endlessh.service &> /dev/null &&
 systemctl stop endlessh.service &> /dev/null &&
 ## Copy endlessh config:
 cp ../config_files/Honeypot/endlessh/config /etc/endlessh/config &&
-echo -e "\n[+] Endlessh installed and configured." || echo -e "\n[-] "
+echo -e "\n[+] Endlessh installed and configured." || echo -e "\n[-] ERROR! Endlessh could not be installed or configured!"
