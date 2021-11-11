a1a3a4e9a70472ae71a25383a612f33df4c51275
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-11 19:08:43 +0100

    Add files via upload

--- install/subscripts/hard-2-ssh-endlessh.sh
+++ install/subscripts/hard-2-ssh-endlessh.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
 ## This script installs and sets up SSH tarpit.
-echo -e "\n################"
-echo -e   "### Endlessh ###"
-echo -e   "################"
+echo -e "\n##########################"
+echo -e   "###      Endlessh      ###"
+echo -e   "##########################"
 
 ## Install endlessh:
-echo -e "[*] Installing Endlessh..."
+echo -e "[*]   Installing Endlessh..."
 apt install endlessh -y &> /dev/null &&
-echo -e "[+] Endlessh installed." || echo -e "[-] ERROR! Endlessh could not be installed!"
+echo -e "[+]   Endlessh installed." || echo -e "[-] ! ERROR! Endlessh could not be installed!"
 
 # Copy endlessh config:
-echo -e "[*] Copying configuration files from '../config_files/Honeypot/endlessh/config' to '/etc/endlessh/config'..."
+echo -e "[*]   Copying configuration files from '../config_files/Honeypot/endlessh/config' to '/etc/endlessh/config'..."
 cp ../config_files/Honeypot/endlessh/config /etc/endlessh/config &&
-echo -e "[+] Endlessh settings copied to '/etc/endlessh/config'." || echo -e "[-] ERROR! Endlessh settings could not be copied to '/etc/endlessh/config'!"
+echo -e "[+]   Endlessh settings copied to '/etc/endlessh/config'." || echo -e "[-] ! ERROR! Endlessh settings could not be copied to '/etc/endlessh/config'!"
 
 ### Permit running Endlessh on private ports (< 1024):
-echo -e "[*] Setting up privileges to run Endlessh on private ports (< 1024)..."
+echo -e "[*]   Setting up privileges to run Endlessh on private ports (< 1024)..."
 ## Find endlessh.service (on my system: "/lib/systemd/system/endlessh.service"):
 SYSTEMD_SERVICE=$(find /usr/ -regex ".*endlessh\.service$" 2> /dev/null | grep -iv "wants" | head -n 1) &&
-echo -e "[+] Found Endlessh systemd service at '${SYSTEMD_SERVICE}'." || echo -e "[-] ERROR! No systemd service found for Endlessh!"
+echo -e "[+]   Found Endlessh systemd service at '${SYSTEMD_SERVICE}'." || echo -e "[-] ! ERROR! No systemd service found for Endlessh!"
 ## Edit endlessh.service:
-echo -e "[*] Editing '${SYSTEMD_SERVICE}..."
+echo -e "[*]   Editing '${SYSTEMD_SERVICE}..."
 sed -i "s/^#AmbientCapabilities=CAP_NET_BIND_SERVICE/AmbientCapabilities=CAP_NET_BIND_SERVICE/" ${SYSTEMD_SERVICE} &> /dev/null &&
 sed -i "s/^PrivateUsers=true/#&/" ${SYSTEMD_SERVICE} &> /dev/null &&
-echo -e "[+] Successfully edited settings in '${SYSTEMD_SERVICE}'." || echo -e "[-] ERROR! Could not edit settings in '${SYSTEMD_SERVICE}"
+echo -e "[+]   Successfully edited settings in '${SYSTEMD_SERVICE}'." || echo -e "[-] ! ERROR! Could not edit settings in '${SYSTEMD_SERVICE}"
 
 ## Kill current process instance on default port:
-echo -e "[*] Setting Endlessh for systemd..."
+echo -e "[*]   Setting Endlessh for systemd..."
 systemctl kill endlessh.service &> /dev/null &&
-echo -e "[+] Service 'endlessh.service' instance on default port 2222 was killed." || echo -e "[-] ERROR! Could not kill service 'endlessh.service' instance on default port 2222!"
+echo -e "[+] Service 'endlessh.service' instance on default port 2222 was killed." || echo -e "[-] ! ERROR! Could not kill service 'endlessh.service' instance on default port 2222!"
 ## Enable on startup:
 systemctl enable endlessh.service &> /dev/null &&
-echo -e "[+] Service 'endlessh.service' enabled on startup." || echo -e "[-] Service 'endlessh.service' could not be enabled to run on startup!"
+echo -e "[+]   Service 'endlessh.service' enabled on startup." || echo -e "[-] ! ERROR! Service 'endlessh.service' could not be enabled to run on startup!"
 
 ## Finished:
-echo -e "[*] Endlessh installation finished."
+echo -e "[*]   Endlessh installation finished."
+echo -e   "##########################"
