80bbd02d84ed0c0a8e8b561682650ca09b967612
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-12 01:37:39 +0100

    Add files via upload

--- install/subscripts/system-update.sh
+++ install/subscripts/system-update.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-## This script updates system
+## Author AISK
+## Description: This script updates system.
+## Date Created: November 12, 2021
+## Last Updated: November 12, 2021
 
 ## Update system:
-apt clean &> /dev/null &&
+#apt clean -y &> /dev/null &&
+apt autoclean -y &> /dev/null &&
+echo -e "[+]   Cleaned." || echo -e "[-] ! ERROR! Could not clean!"
+
 apt update &> /dev/null &&
+echo -e "[+]   Updated." || echo -e "[-] ! ERROR! Could not update!"
+
+apt upgrade &> /dev/null &&
+echo -e "[+]   Upgraded." || echo -e "[-] ! ERROR! Could not upgrade!"
+
 apt full-upgrade -y &> /dev/null &&
-apt dist-upgrade -y &> /dev/null &&
-apt install apt-file -y &> /dev/null &&
-apt-file update &> /dev/null &&
+echo -e "[+]   Fully upgraded." || echo -e "[-] ! ERROR! Could not fully upgrade!"
+
 apt autoremove &> /dev/null &&
-echo -e "\n[+] Full system updated!" || echo -e "\n[-] ERROR! System could not be fully updated!"
+echo -e "\n[+]   Removed no longer needed packages." || echo -e "\n[-] ! ERROR! Could not remove no longer needed packages!"
+
+## Script end banner:
+echo -e   "##########################"
