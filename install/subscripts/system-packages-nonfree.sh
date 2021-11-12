d14bf117abab41301efcbe269781e1f57f405f1f
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-12 01:19:36 +0100

    Add files via upload

--- install/subscripts/system-packages-nonfree.sh
+++ install/subscripts/system-packages-nonfree.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-## This script adds contrib and non-free packages to debian:
+## Author AISK
+## Description: This script adds contrib and non-free packages to debian:
+## Date Created: November 12, 2021
+## Last Updated: November 12, 2021
+
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "### Add non-free pkgs  ###"
+echo -e   "##########################"
 
 ## Add contrib and non-free packages to debian:
 sed -i 's/^deb.*main$/& contrib non-free/g' /etc/apt/sources.list &&
-echo -e "\n[+] Added contrib and non-free packages to '/etc/apt/sources.list'." || echo -e "\n[-] ERROR! Could not add contrib and non-free packages to '/etc/apt/sources.list'!"
+echo -e "[+]   Added contrib and non-free packages to '/etc/apt/sources.list'." || echo -e "[-] ! ERROR! Could not add contrib and non-free packages to '/etc/apt/sources.list'!"
+
+## Script end banner:
+echo -e   "##########################"
