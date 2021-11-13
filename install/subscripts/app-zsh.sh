c945b95a65ae10eb973932c84a5550f93815f724
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-13 19:28:10 +0100

    Add files via upload

--- install/subscripts/app-zsh.sh
+++ install/subscripts/app-zsh.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-## This script installs zsh and zsh related pkgs and set zsh as default shell for ${USER}:
+## Author AISK
+## Description: This script installs zsh and zsh related pkgs and set zsh as default shell for ${USER}.
+## Date Created: November 13, 2021
+## Last Updated: November 13, 2021
+
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "###         ZSH        ###"
+echo -e   "##########################"
 
 ## Get user from a command line:
 USER="${1}"
 
 ## Install ZSH and ZSH related pkgs:
 apt install zsh zsh-autosuggestions zsh-syntax-highlighting -y &> /dev/null &&
+echo -e "[+]   Packages 'zsh zsh-autosuggestions zsh-syntax-highlighting' were installed." || echo -e "[-] ! ERROR. Packages 'zsh zsh-autosuggestions zsh-syntax-highlighting' could not be installed!"
+
+## Set ZSH as default shell for ${USER}:
 usermod -s /bin/zsh ${USER} &> /dev/null &&
-echo -e "\n[+] ZSH related packages were installed and ZSH was set as default shell for user '${USER}'." || echo -e "\n[-] ERROR! Either ZSH related packages were not installed or ZSH was not set as default shell for user '${USER}'!"
+echo -e "[+]   Set ZSH as default shell for '${USER}'." || echo -e "[-] ! ERROR! Could not set zsh as default shell for '${USER}'!"
+
+## Script end banner:
+echo -e   "##########################"
