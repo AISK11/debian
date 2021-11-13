f7b8581d1fe72b16124026588789f20cdd4c226e
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-13 19:34:39 +0100

    Add files via upload

--- install/subscripts/app-audio.sh
+++ install/subscripts/app-audio.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-## This script install audio control packages:
+## Author AISK
+## Description: This script install audio control packages.
+## Date Created: November 13, 2021
+## Last Updated: November 13, 2021
+
+echo -e "\n##########################"
+echo -e   "###        Audio       ###"
+echo -e   "##########################"
 
 ## Install audio control:
 apt install alsa-utils -y &> /dev/null &&
+echo -e "[+]   Package 'alsa-utils' was installed." || echo -e "[-] ! ERROR! Package 'alsa-utils' could not be installed!"
 
 ## To fix mic issue:
 apt install pulseaudio -y &> /dev/null &&
-echo -e "\n[+] Audio control packages were installed." || echo -e "\n[-] ERROR! Audio control packages could not be installed!"
+echo -e "[+]   Package 'pulseaudio' was installed (to fix mic issues)." || echo -e "[-] ! ERROR! Package 'pulseaudio' could not be installed (Mic issues can appear)!"
+
+## Script end banner:
+echo -e   "##########################"
