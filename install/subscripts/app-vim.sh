ee88213d8083d3cd1776599f55ac2059199113af
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-13 19:23:38 +0100

    Add files via upload

--- install/subscripts/app-vim.sh
+++ install/subscripts/app-vim.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-## This script installs 'vim' and 'bvi' and sets 'vim' as default editor:
+## Author AISK
+## Description: This script installs 'vim' and 'bvi' and sets 'vim' as default editor.
+## Date Created: November 13, 2021
+## Last Updated: November 13, 2021
+
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "###         Vim        ###"
+echo -e   "##########################"
 
 ## Install 'vim' and 'bvi':
 apt install vim bvi -y &> /dev/null &&
+echo -e "[+]   Packages 'vim' and 'bvi' were installed." || echo -e "[-] ! ERROR! Packages 'vim' and 'bvi' could not be installed!"
+
 ## Set 'vim' as default editor:
 update-alternatives --set editor /usr/bin/vim.basic &> /dev/null &&
-echo -e "\n[+] 'vim' and 'bvi' installed and 'vim' was set as a default editor." || echo -e "\n[-] ERROR! Either 'vim' and 'bvi' could not be installed or 'vim' was not set as default editor!"
+echo -e "[+]   Vim was set as default editor'." || echo -e "[-] ! ERROR! Vim could not be set as default editor!"
+
+## Script end banner:
+echo -e   "##########################"
