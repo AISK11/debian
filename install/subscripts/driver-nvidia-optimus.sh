d8ceeef96bb801fe099487e03dbe5be83f2d1060
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-14 00:44:54 +0100

    Add files via upload

---
+++ install/subscripts/driver-nvidia-optimus.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## Author AISK
+## Description: This script install Nvidia drivers and other Nvidia tools.
+## Date Created: November 13, 2021
+## Last Updated: November 13, 2021
+
+## Script start banner:
+echo -e "\n##########Driver##########"
+echo -e   "###   Nvidia Optimus   ###"
+echo -e   "##########################"
+
+## Install Nvidia driver and Nvidia tools:
+apt install intel-gpu-tools nvtop nvidia-detect linux-headers-amd64 nvidia-driver firmware-misc-nonfree -y &> /dev/null &&
+echo -e "[+]   Packages 'intel-gpu-tools nvtop nvidia-detect linux-headers-amd64 nvidia-driver firmware-misc-nonfree' were installed." || echo -e "[-] ! ERROR! Packages 'intel-gpu-tools nvtop nvidia-detect linux-headers-amd64 nvidia-driver firmware-misc-nonfree' could not be installed!"
+
+## Set Xorg config for Nvidia Optimus:
+cp ../config_files/X11/xorg.conf /etc/X11/xorg.conf &&
+echo -e "[+]   Xorg config file was set in '/etc/X11/xorg.conf'." || echo -e "[-] ! ERROR! Xorg config file could not be set in '/etc/X11/xorg.conf'!"
+
+## Script end banner:
+echo -e   "##########################"
