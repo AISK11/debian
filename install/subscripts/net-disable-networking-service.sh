ad3eded6a8214b46a2edf41b32f677e8f25513c5
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-13 17:52:47 +0100

    Add files via upload

--- install/subscripts/net-disable-networking-service.sh
+++ install/subscripts/net-disable-networking-service.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-## This script disables unnecessary networking service:
-systemctl disable networking.service &> /dev/null &&
-echo -e "\n[+] 'networking.service' disabled." || echo -e "\n[-] ERROR! Could not disable 'networking.service'!"
+## Author AISK
+## Description: This script disables unnecessary networking service.
+## Date Created: November 13, 2021
+## Last Updated: November 13, 2021
+
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "### Networking.service ###"
+echo -e   "##########################"
+
+systemctl disable networking.service 1> /dev/null &&
+echo -e "[+]   Service 'networking.service' disabled." || echo -e "[-] ! ERROR! Service 'networking.service' could not be disabled!"
+
+## Script end banner:
+echo -e   "##########################"
