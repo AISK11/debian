04f004e8e4c70b62502a9d62b47d1ddacd20dc53
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-13 20:11:03 +0100

    Add files via upload

--- install/subscripts/x11-i3-utils.sh
+++ install/subscripts/x11-i3-utils.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-## This script install other X11 i3 utilities.
+## Author AISK
+## Description: This script install other X11 i3 utilities.
+## Date Created: November 13, 2021
+## Last Updated: November 13, 2021
+
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "###      X11 utils     ###"
+echo -e   "##########################"
 
 ## Install X11 i3 utilities:
 apt install i3blocks i3lock numlockx rofi feh scrot compton light xclip lxappearance -y &> /dev/null &&
-echo -e "\n[+] i3 utilities were installed." || echo -e "\n[-] ERROR! i3 utilities could not be installed!"
+echo -e "[+]   Packages 'i3blocks i3lock numlockx rofi feh scrot compton light xclip lxappearance' were installed." || echo -e "[-] ! ERROR! Packages 'i3blocks i3lock numlockx rofi feh scrot compton light xclip lxappearance' could not be installed!"
+
+## Script end banner:
+echo -e   "##########################"
