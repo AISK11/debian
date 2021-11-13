4bc3ac3d6c9ae088bb4b7b916ac5f10c3a9c9b3b
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-13 19:36:48 +0100

    Add files via upload

--- install/subscripts/app-git.sh
+++ install/subscripts/app-git.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-## This script installs git:
+## Author AISK
+## Description: This script installs git.
+## Date Created: November 13, 2021
+## Last Updated: November 13, 2021
+
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "###         Git        ###"
+echo -e   "##########################"
 
 ## Install git:
 apt install git -y &> /dev/null &&
-echo -e "\n[+] Git installed." || echo -e "\n[-] ERROR! Git was not installed!"
+echo -e "[+]   Package 'git' was installed." || echo -e "[-] ! ERROR! Package 'git' could not be installed!"
+
+## Script end banner:
+echo -e   "##########################"
