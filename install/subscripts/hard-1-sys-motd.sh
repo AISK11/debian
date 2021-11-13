6f984753ab9c5c419f6465c1d79e3c99c045565b
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-14 00:44:13 +0100

    Add files via upload

--- install/subscripts/hard-1-sys-motd.sh
+++ install/subscripts/hard-1-sys-motd.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-## This script sets MOTD which is executed when logging in to be blank:
+## Author AISK
+## Description: This script sets MOTD which is executed when logging in to be blank.
+## Date Created: November 14, 2021
+## Last Updated: November 14, 2021
+
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "###     Blank MOTD     ###"
+echo -e   "##########################"
 
 ## Set blank MOTD:
 echo "" > /etc/issue &&
-echo -e "\n[+] MOTD was cleared in '/etc/issue'!" || echo -e "\n[-] ERROR! MOTD could not be cleared in '/etc/issue'!"
+echo -e "[+]   MOTD was cleared in '/etc/issue'!" || echo -e "[-] ! ERROR! MOTD could not be cleared in '/etc/issue'!"
+
+## Script end banner:
+echo -e   "##########################"
