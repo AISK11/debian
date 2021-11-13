80077f6a1e220c442739516cbf889bedc38a344e
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-14 00:52:46 +0100

    Add files via upload

--- install/subscripts/hard-1-sys-usbguard.sh
+++ install/subscripts/hard-1-sys-usbguard.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-## This script installs USB Guard.
+## Author AISK
+## Description: This script installs USB Guard.
+## Date Created: November 14, 2021
+## Last Updated: November 14, 2021
+
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "###      USB Guard     ###"
+echo -e   "##########################"
 
 ## Install USB Guard:
 apt install usbguard -y &> /dev/null &&
+echo -e "[+]   Package 'usbguard' installed." || echo -e "[-] ! ERROR! Could not install package 'usbguard'."
+
+## Enable usbguard on startup:
 systemctl enable usbguard.service &> /dev/null &&
-echo -e "\n[+] USB Guard installed." || echo -e "\n[-] ERROR! USB Guard could not be installed!"
+echo -e "[+]   Service 'usbguard.service' enabled on startup." || echo -e "[-] ! ERROR! Service 'usbguard.service' could not be enabled on startup!"
+
+## Script end banner:
+echo -e   "##########################"
