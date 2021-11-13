1ce064136570ba7ced2b3155caa32641ec917b78
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-13 18:40:41 +0100

    Add files via upload

--- install/subscripts/net-dns-servers.sh
+++ install/subscripts/net-dns-servers.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-## This script sets DNS servers:
+## Author AISK
+## Description: This script sets DNS servers.
+## Date Created: November 13, 2021
+## Last Updated: November 13, 2021
 
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "###     DNS servers    ###"
+echo -e   "##########################"
+
+## Copy DNS server list:
 cp ../config_files/DNS/resolv.conf /etc/resolv.conf &&
-echo -e "\n[+] DNS servers set in '/etc/resolv.conf'." || echo -e "ERROR! DNS servers could not be set in '/etc/resolv.conf'!"
+echo -e "[+]   DNS servers set in '/etc/resolv.conf'." || echo -e "[-] ! ERROR! DNS servers could not be set in '/etc/resolv.conf'!"
+
+## Script end banner:
+echo -e   "##########################"
