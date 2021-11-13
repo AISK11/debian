a8715f2e59a21d140bd1a8a9670c6c080caa7fe2
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-13 21:21:10 +0100

    Add files via upload

--- install/subscripts/driver-network-iwlwifi.sh
+++ install/subscripts/driver-network-iwlwifi.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-## This script installs iwlwifi drivers.
+## Author AISK
+## Description: This script installs iwlwifi drivers (Intel Wi-Fi).
+## Date Created: November 13, 2021
+## Last Updated: November 13, 2021
 ## Requires: non-free and contrib packages.
+
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "###Driver - Intel Wi-Fi###"
+echo -e   "##########################"
 
 ## Driver for iwlwifi:
 apt install firmware-iwlwifi -y &> /dev/null &&
-echo -e "\n[+] Installed firmware for iwlwifi." || echo -e "\n[-] ERROR! Could not  install iwlwifi driver!"
+echo -e "[+]   Package 'firmware-iwlwifi' (Intel Wi-Fi) installed." || echo -e "[-] ! ERROR! Package 'firmware-iwlwifi' (Intel Wi-Fi) could not be installed!"
+
+## Script end banner:
+echo -e   "##########################"
