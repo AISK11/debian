f1b3d00b2c45e80369cc29af85ff05de69f5f174
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-13 19:15:48 +0100

    Update net-dhcp-client.sh

--- install/subscripts/net-dhcp-client.sh
+++ install/subscripts/net-dhcp-client.sh
@@ -15,12 +15,12 @@ apt install dhcpcd5 -y &> /dev/null &&
 echo -e "[+]   Package 'dhcpcd5' was installed." || echo -e "[-] ! ERROR! Could not install package 'dhcpcd5'!"
 
 ## Do not start DHCPCD on startup:
-systemctl disable dhcpcd.service 1> /dev/null &&
+systemctl disable dhcpcd.service &> /dev/null &&
 echo -e "[+]   Disabled autostart for service 'dhcpcd.service'." || echo -e "[-] ! ERROR! Could not disable autostart for service 'dhcpcd.service'!"
 
 ## Copy DHCPCD config:
 cp ../config_files/DHCP/dhcpcd.conf /etc/dhcpcd.conf &&
-echo -e "[+]   Config file for dhcpcd was copied to '/etc/dhcpcd.conf'" || echo -e "[-] ! ERROR! Config file for dhcpcd could not be copied to '/etc/dhcpcd.conf'!"
+echo -e "[+]   Config file for dhcpcd was copied to '/etc/dhcpcd.conf'." || echo -e "[-] ! ERROR! Config file for dhcpcd could not be copied to '/etc/dhcpcd.conf'!"
 
 ## Uninstall previous DHCP Client:
 dpkg --purge isc-dhcp-client isc-dhcp-common &> /dev/null &&
