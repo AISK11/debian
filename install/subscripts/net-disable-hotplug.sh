5fa23f76547c006b497a3cd6144028852c669940
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-13 18:23:13 +0100

    Add files via upload

--- install/subscripts/net-disable-hotplug.sh
+++ install/subscripts/net-disable-hotplug.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-## This script disables autostart and autoconnect of network interfaces:
+## Author AISK
+## Description: This script disables autostart and autoconnect of network interfaces.
+## Date Created: November 13, 2021
+## Last Updated: November 13, 2021
+
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "###  Interface hotplug ###"
+echo -e   "##########################"
 
 ## Global interface list:
 declare -a INTERFACE_LIST=("eth0" "wlan0")
@@ -8,11 +16,14 @@ declare -a INTERFACE_LIST=("eth0" "wlan0")
 ## Loopback is specific interface and it should autostart:
 echo -e "auto lo" > /etc/network/interfaces &&
 echo -e "iface lo inet loopback" >> /etc/network/interfaces &&
-echo -e "\n[+] Loopback interface 'lo' was successfully set up in '/etc/network/interfaces'." || echo -e "\n[-] ERROR! Loopback interface 'lo0' could not be set up!"
+echo -e "[+]   Loopback interface 'lo' was successfully set to autostart in '/etc/network/interfaces'." || echo -e "[-] ! ERROR! Loopback interface 'lo0' could not be set to autostart in '/etc/network/interfaces'!"
 
 ## Disable interfaces for auto connectivity:
 for INTERFACE in "${INTERFACE_LIST[@]}"; do
     echo -e "#allow ${INTERFACE} hotplug" >> /etc/network/interfaces &&
     echo -e "iface ${INTERFACE} inet manual" >> /etc/network/interfaces &&
-    echo -e "[+] Interface '${INTERFACE}' disabled for autoconnectivity in '/etc/network/interfaces'." || echo -e "[-] ERROR! Interface '${INTERFACE}' could not be disabled for autoconnectivity in '/etc/network/interfaces'!"
+    echo -e "[+]   Interface '${INTERFACE}' disabled autostart in '/etc/network/interfaces'." || echo -e "[-] ! ERROR! Interface '${INTERFACE}' could not be disabled for autostart in '/etc/network/interfaces'!"
 done
+
+## Script end banner:
+echo -e   "##########################"
