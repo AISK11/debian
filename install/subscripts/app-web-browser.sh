0b6110a134ce0bcee97383defa1c4b3abe359ee4
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-13 19:31:22 +0100

    Add files via upload

--- install/subscripts/app-web-browser.sh
+++ install/subscripts/app-web-browser.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-## This script install firefox and set as default browser:
+## Author AISK
+## Description: This script install firefox and set as default browser.
+## Date Created: November 13, 2021
+## Last Updated: November 13, 2021
+
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "###     Web Browser    ###"
+echo -e   "##########################"
 
 ## Install firefox:
 apt install firefox-esr -y &> /dev/null &&
+echo -e "[+]   Package 'firefox-esr' was installed." || echo -e "[-] ! ERROR! Package 'firefox-esr' could not be installed!"
 
 ## Set firefox as default web browser:
 update-alternatives --set x-www-browser /usr/bin/firefox-esr &> /dev/null &&
-echo -e "\n[+] Firefox installed and set as default browser." || echo -e "\n[-] ERROR! Either Firefox was not installed or was not set as default web browser!"
+echo -e "[+]   Firefox was set as default x-www-browser." || echo -e "[-] ! ERROR! Could not set Firefox as default x-www-browser!"
+
+## Script end banner:
+echo -e   "##########################"
