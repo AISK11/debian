3f2b878b088768410ffeb3fb4d8b52783091df77
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-13 20:13:52 +0100

    Add files via upload

--- install/subscripts/x11-urxvt.sh
+++ install/subscripts/x11-urxvt.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-## This script installs URXVT Terminal emulator and set it as default Terminal emulator.
+## Author AISK
+## Description: This script installs URXVT Terminal emulator and set it as default Terminal emulator.
+## Date Created: November 13, 2021
+## Last Updated: November 13, 2021
+
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "###        URXVT       ###"
+echo -e   "##########################"
 
 ## Install URXVT Terminal emulator:
 apt install rxvt-unicode-256color -y &> /dev/null &&
+echo -e "[+]   Package 'rxvt-unicode-256color' was installed." || echo -e "[-] ! ERROR! Package 'rxvt-unicode-256color' could not be installed!"
+
 ## Set URXVT as default Termianl Emulator:
 update-alternatives --set x-terminal-emulator /usr/bin/urxvt &> /dev/null &&
-echo -e "\n[+] 'rxvt-unicode-256color' installed and set as default X-terminal." || echo -e "\n[-] ERROR! Either 'rxvt-unicode-256color' was not installed or was not set as default terminal emulator!"
+echo -e "[+]   URXVT was set as default x-terminal-emulator." || echo -e "[-] ! ERROR! Could not set URXVT as default x-terminal-emulator!"
+
+## Script end banner:
+echo -e   "##########################"
