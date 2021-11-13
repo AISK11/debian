b6c1b05ee47f49460aafd30486b0328897ef7569
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-13 19:47:03 +0100

    Add files via upload

--- install/subscripts/x11-xorg.sh
+++ install/subscripts/x11-xorg.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-## This script install Xorg and xinit to be able to execute WM or DE:
+## Author AISK
+## Description: This script install Xorg and xinit to be able to execute WM or DE.
+## Date Created: November 13, 2021
+## Last Updated: November 13, 2021
+
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "###        Xorg        ###"
+echo -e   "##########################"
 
 ## Install xorg and xinit packages:
 apt install xorg x11-xserver-utils xinit -y &>/dev/null &&
-echo -e "\n[+] Xorg installed." || echo -e "\n[-] ERROR! Xorg was not installed successfully!"
+echo -e "[+]   Packages 'x11-xserver-utils xinit' were installed." || echo -e "[-] ! ERROR! Packages 'x11-xserver-utils xinit' could not be installed!"
+
+## Script end banner:
+echo -e   "##########################"
+
