a82288a5a676027e143656b89264c96424637129
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-14 01:02:00 +0100

    Add files via upload

--- install/subscripts/hard-1-sys-addkalirepo.sh
+++ install/subscripts/hard-1-sys-addkalirepo.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
-## This script adds kali repository.
+## Author AISK
+## Description: This script adds kali repository.
+## Date Created: November 14, 2021
+## Last Updated: November 14, 2021
+
+## Script start banner:
+echo -e "\n##########################"
+echo -e   "### Add Kali repository###"
+echo -e   "##########################"
 
 ## Add kali repository:
 apt install gnupg -y &> /dev/null &&
+echo -e "[+]   Package 'gnupg' installed." || echo -e "[-] ! ERROR! Package 'gnupg' could not be installed!"
+
+## Add kali repository to debian sources:
 bash -c 'echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list' &&
+echo -e "[+]   Kali repository added to '/etc/apt/sources.list'." || echo -e "[-] ! ERROR! Could not add kali repository to '/etc/apt/sources.list'!"
+
+## Add kali key for kali repo:
 wget 'https://archive.kali.org/archive-key.asc' &> /dev/null &&
 apt-key add "./archive-key.asc" &> /dev/null &&
+rm "./archive-key.asc" &&
+echo -e "[+]   Added kali key from 'https://archive.kali.org/archive-key.asc'." || echo -e "[-] ! ERROR! Could not add kali key from 'https://archive.kali.org/archive-key.asc'!"
+
 ## Assign low prio to kali pkgs:
 touch '/etc/apt/preferences' &&
 echo "Package: *" > '/etc/apt/preferences' &&
 echo "Pin: release a=kali-rolling" >> '/etc/apt/preferences' &&
 echo "Pin-Priority: 50" >> '/etc/apt/preferences' &&
-rm "./archive-key.asc" &&
-echo -e "\n[+] Kali repository added!" || echo -e "\n[-] ERROR! Kali repository could not be added!"
+echo -e "[+]   Assigned low priority (50) for kali packages in '/etc/apt/preferences'." || echo -e "[-]   Low priority (50) could not be assigned for kali packages in '/etc/apt/preferences'!"
+
+## Script end banner:
+echo -e   "##########################"
