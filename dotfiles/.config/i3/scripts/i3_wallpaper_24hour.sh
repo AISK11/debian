e83603087fed2c680b141ac2ca429f9103753371
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-15 16:40:13 +0100

    Add files via upload

---
+++ dotfiles/.config/i3/scripts/i3_wallpaper_24hour.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## This script sets background according to datetime:
+
+## Image location directory
+## Images should be in form: 00.png - 23.png:
+DIRECTORY="${HOME}/.config/i3/24hour/forest"
+
+### Add crontab rules to user's cron to execute each hour.
+## Note: grep returns 0 on match, 1 otherwise.
+## If crontab rule does not exist, create it:
+if grep "^0 * * * * ${HOME}/.config/i3/scripts/i3_wallpaper_daytime.sh"
+  /var/spool/cron/crontabs/$(whoami) &> /dev/null; then
+    echo "[*]   Reboot rule for services 'logrotate.service rsyslog.service' already exists."
+else
+    echo -e "0 * * * * ${HOME}/.config/i3/scripts/i3_wallpaper_daytime.sh" >> /var/spool/cron/crontabs/$(whoami) &&
+fi
+## Restart cron service:
+#systemctl restart cron.service &&
+#echo -e "[+]   Service 'cron.service' were restarted." || echo -e "[-] ! ERROR! Service 'cron.service' could not be restarted!"
+
+## Get current hour:
+HOUR=$(date "+%H") ## 00 - 23
+
+## Set wallpaper:
+if [[ -f "${HOME}/.config/i3/24hour/forest/${HOUR}.png" ]]; then
+    feh --bg-scale --force-aliasing "${HOME}/.config/i3/24hour/forest/${HOUR}.png"
+fi
