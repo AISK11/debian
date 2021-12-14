cc52009a86b36767391a5b3d4c641eeb9f9b733a
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-12-14 01:39:17 +0100

    Update run_wlan0.sh

--- dotfiles/scripts/run_wlan0.sh
+++ dotfiles/scripts/run_wlan0.sh
@@ -14,7 +14,7 @@
 INTERFACE="wlan0"
 
 ## Bring everything down in case of a restart:
-doas dhcpcd --release ${INTERFACE} && echo "[+] DHCP released for interface '${INTERFACE}'." || echo "[-] ERROR! DHCP was not released for interface '${INTERFACE}'!"
+doas dhcpcd --release ${INTERFACE} && echo "[+] DHCP released for interface '${INTERFACE}'." || echo "[*] DHCP was not released for interface '${INTERFACE}'."
 doas ip a flush ${INTERFACE} && echo "[+] IP flushed for interface '${INTERFACE}'." || echo "[-] ERROR! IP was not flushed for interface '${INTERFACE}'!"
 doas systemctl stop dhcpcd.service && echo "[+] Service 'dhcpcd.service' stopped." || echo "[-] ERROR! Service 'dhcpcd.service' was not stopped!"
 doas systemctl stop wpa_supplicant.service && echo "[+] Service 'wpa_supplicant.service' stopped." || echo "[-] ERROR! Service 'wpa_supplicant.service' was not stopped!"
@@ -25,7 +25,7 @@ doas rfkill unblock wlan && echo "[+] WLAN unblocked in rfkill." || echo "[-] ER
 doas macchanger -A ${INTERFACE} && echo "[+] MAC address changed for interface '${INTERFACE}'." || echo "[-] ERROR! MAC address for interface '${INTERFACE}' was not changed!"
 doas rm -rf /var/lib/dhcpcd/* && echo "[+] All leased addresses were removed in directory '/var/lib/dhcpcd/'." || echo "[-] ERROR! Lease addresses in directory '/var/lib/dhcpcd/' were not removed!"
 doas rm -f /run/wpa_supplicant/${INTERFACE} && echo "[+] WPA_SUPPLICANT file '/run/wpa_supplicant/${INTERFACE}' was removed." || echo "[-] ERROR! WPA_SUPPLICANT file '/run/wpa_supplicant/${INTERFACE}' was not removed."
-doas killall -9 wpa_supplicant && echo "[+] All instances of process 'wpa_supplicant' were killed." || echo "[-] ERROR! Could not kill all instances of process 'wpa_supplicant'!"
+doas killall -9 wpa_supplicant && echo "[+] All instances of process 'wpa_supplicant' were killed." || echo "[*] Could not kill all instances of process 'wpa_supplicant'. There are no processes running."
 doas ip l set ${INTERFACE} up && echo "[+] Interface '${INTERFACE}' state is UP." || echo "[-] ERROR! Interface '${INTERFACE}' state was not set to UP!"
 doas systemctl start wpa_supplicant.service && echo "[+] Service 'wpa_supplicant.service' started." || echo "[-] ERROR! Service 'wpa_supplicant.service' was not started!"
 doas systemctl start dhcpcd.service && echo "[+] Service 'dhcpcd.service' started." || echo "[-] ERROR! Sercice 'dhcpcd.service' was not started!"
